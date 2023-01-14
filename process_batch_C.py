ab987cf7e4aae8dda34485679fab69e5c165f304
Amanda <20828566+mandi1120@users.noreply.github.com>
2023-01-14 12:50:03 -0600

    updated files

--- process_batch_C.py
+++ process_batch_C.py
@@ -12,3 +12,27 @@ import csv
 input_file_name = "batchfile_2_kelvin.csv"
 output_file_name = "batchfile_3_farenheit.csv"
 
+#open the input and output files
+input_file = open(input_file_name, "r")
+output_file = open(output_file_name, "w", newline='')
+
+reader = csv.reader(input_file, delimiter=",")
+writer = csv.writer(output_file, delimiter=",")
+
+header = next(reader)
+header_list = ["Year","Month","Day","Time","TempF"]
+writer.writerow(header_list)
+
+for row in reader:
+    #access the kelvin temp file
+    Year, Month, Day, Time, TempK = row
+
+    #convert kelvin to fahrenheit using this formula: 
+    # f = (K − 273.15) × 9/5 + 32  
+    TempF = round((float(TempK)-273.15) * 9/5 + 32, 2)
+
+    #write the fahrenheit temps to the new output file
+    writer.writerow([Year, Month, Day, Time, TempF])
+
+output_file.close()
+input_file.close()
