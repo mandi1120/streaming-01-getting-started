07062de06b8722d4353b2e24cebdb55145a12adc
Amanda <20828566+mandi1120@users.noreply.github.com>
2023-01-15 16:35:41 -0600

    update file

--- process_batch_C.py
+++ process_batch_C.py
@@ -1,14 +1,19 @@
 """
+Amanda Hanway 
+Assignment A1: Setup and Datagram Sockets
+
 Batch Process - third transformation
 
 Reads from a file, transform, write to a new file.
 
 In this case, covert degree K to degree F.
+Formula: f = (K − 273.15) × 9/5 + 32  
 
 """
 
 import csv
 
+# set file names
 input_file_name = "batchfile_2_kelvin.csv"
 output_file_name = "batchfile_3_farenheit.csv"
 
@@ -16,9 +21,11 @@ output_file_name = "batchfile_3_farenheit.csv"
 input_file = open(input_file_name, "r")
 output_file = open(output_file_name, "w", newline='')
 
+# create file reader and writer
 reader = csv.reader(input_file, delimiter=",")
 writer = csv.writer(output_file, delimiter=",")
 
+# define header column names and write to output file
 header = next(reader)
 header_list = ["Year","Month","Day","Time","TempF"]
 writer.writerow(header_list)
@@ -34,5 +41,6 @@ for row in reader:
     #write the fahrenheit temps to the new output file
     writer.writerow([Year, Month, Day, Time, TempF])
 
+# close files
 output_file.close()
 input_file.close()
